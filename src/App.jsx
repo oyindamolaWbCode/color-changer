1b55e1831a59fe8496cc03a5f7fb2b3fb0bc99b7
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-09-05 18:07:07 +0100

    page UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ function App() {
         <div className="nav-details">
           <h3>COLOR FLIPPER</h3>
           <div className="color-range">
-          <a href='#' style={{marginRight: '8px'}}> Simple </a> 
-          <a href='#'> Hex </a> 
+            <a href="#" style={{ marginRight: "8px" }}>
+              
+              Simple
+            </a>
+            <a href="#"> Hex </a>
           </div>
         </div>
         <div className="color-body">
           <div className="TheBody">
-            <div className="color-name">
             <p>Background Color : Black</p>
-            </div>
-            <div className="buton">
+
             <button>Change Color</button>
-            </div>
           </div>
         </div>
       </div>
